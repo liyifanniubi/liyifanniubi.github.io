3a04e4eaf254b8676ae61efeaf11eec9b23fee60
liyifanniubi <null>
2024-07-20 23:10:51 +0000

    🎉auto update by Gmeek action

--- docs/assets/lazyload.js
+++ docs/assets/lazyload.js
@@ -1,17 +1,21 @@
-const ob = new IntersectionObserver((entris) => {
-    entris.forEach(entry => {
-        if (entry.isIntersecting) {
-            const img = entry.target
-            img.src = img.dataset.src;
-            ob.unobserve(img);
-        }
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const ob = new IntersectionObserver((entris) => {
+        entris.forEach(entry => {
+            if (entry.isIntersecting) {
+                const img = entry.target
+                img.src = img.dataset.src;
+                ob.unobserve(img);
+            }
+        });
+    },
+    {
+        rootMargin: '0px 0px 500px 0px',
     });
-},
-{
-    rootMargin: '0px 0px 200px 0px',
-});
 
-const imgs = document.querySelectorAll('img[data-src]');
-imgs.forEach(img => {
-    ob.observe(img);
+    const imgs = document.querySelectorAll('img[data-src]');
+    imgs.forEach(img => {
+        ob.observe(img);
+    });
+
 });
