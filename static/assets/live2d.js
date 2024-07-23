7b08ac0b5a0d422f428dd81cfb7e1a25b6bbf5ff
liyifanniubi <liangchishi8@gmail.com>
2024-07-23 15:25:11 +0800

    Create live2d.js

---
+++ static/assets/live2d.js
@@ -0,0 +1,6 @@
+if (window.innerWidth >= 1250) {
+    var script = document.createElement('script');
+    script.type = 'text/javascript';
+    script.src = 'https://blog.liyifan.xyz/live2d/autoload.js';
+    document.body.appendChild(script);
+}
