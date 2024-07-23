a7cd326f45f85e4baa7ec89ea49890ef1d9c2425
liyifanniubi <null>
2024-07-23 05:12:09 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [8](https://blog.liyifan.xyz/tag.html) 
 ### :speech_balloon: 1 
 ### :hibiscus: 32940 
-### :alarm_clock: 2024-07-23 13:08:57 
+### :alarm_clock: 2024-07-23 13:12:09 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/live2d/autoload.js
+++ docs/live2d/autoload.js
@@ -35,7 +35,7 @@ if (screen.width >= 768) {
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			cdnPath: "https://blog.liyifan.xyz/live2d/live2d_api/",
 			tools: ["switch-model","quit"]
 		});
 	});
