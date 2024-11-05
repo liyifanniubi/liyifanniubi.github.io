60e2a5d196ba647ad97b2192f1e0607e066f4f78
liyifanniubi <liangchishi8@gmail.com>
2024-11-05 19:51:17 +0800

    Update viewimg.js

--- static/assets/viewimg.js
+++ static/assets/viewimg.js
@@ -1,11 +1,12 @@
 document.addEventListener("DOMContentLoaded", function() {
     // 加载viewImage.js库
-    let Script = document.createElement('script');
-    Script.type = 'text/javascript';
-    Script.src = 'https://blog.liyifan.xyz/lib/view-image.js';
-    document.body.appendChild(Script);
+    let viewImage = document.createElement('script');
+    viewImage.type = 'text/javascript';
+    viewImage.src = 'https://blog.liyifan.xyz/lib/view-image.js';
+    document.body.appendChild(viewImage);
 
     let initial = document.createElement('script');
     initial.type = 'text/javascript';
     initial.textContent = 'window.ViewImage && ViewImage.init(\'img\');';
     document.body.appendChild(initial);
+}
