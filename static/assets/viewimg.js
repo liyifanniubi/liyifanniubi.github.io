de4851faf7c1137906bb5184b30112baa872245e
liyifanniubi <liangchishi8@gmail.com>
2025-07-23 03:00:28 +0800

    Update viewimg.js

--- static/assets/viewimg.js
+++ static/assets/viewimg.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     // 加载viewImage.js库
     let viewImage = document.createElement('script');
-    viewImage.src = 'https://blog.liyifan.xyz/lib/view-image.js';
+    viewImage.src = 'https://blog.154451.xyz/lib/view-image.js';
     document.body.appendChild(viewImage);
 
     viewImage.onload = function() {
