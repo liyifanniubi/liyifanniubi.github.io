806630e4293256411d684a0fc05073b6534d9f15
liyifanniubi <null>
2025-07-22 19:02:44 +0000

    🎉auto update by Gmeek action

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 ### :page_facing_up: [12](https://blog.154451.xyz/tag.html) 
 ### :speech_balloon: 2 
 ### :hibiscus: 50005 
-### :alarm_clock: 2025-07-23 02:50:56 
+### :alarm_clock: 2025-07-23 03:02:43 
 ### Powered by :heart: [Gmeek](https://github.com/Meekdai/Gmeek)
--- docs/assets/live2d.js
+++ docs/assets/live2d.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     if (window.innerWidth >= 1250) {
         var script = document.createElement('script');
         script.type = 'text/javascript';
-        script.src = 'https://blog.liyifan.xyz/live2d/autoload.js';
+        script.src = 'https://blog.154451.xyz/live2d/autoload.js';
         document.body.appendChild(script);
     }
     
